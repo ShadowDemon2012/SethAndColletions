585f564a1223e14cf55e104c956ad44e3f86fcb7
Dmitriy <shadowgods@mail.ru>
2022-10-02 23:38:42 +0500

    Initial commit

---
+++ src/main/java/pro/sky/sethandcolletions/SethAndCollectionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.sethandcolletions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SethAndColletionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SethAndColletionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/sethandcolletions/SethAndCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.sethandcolletions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SethAndColletionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
